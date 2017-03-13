ac04dab04bd614e1479560cbcdd9bf2f1593ceb6
dunn1pt@cmich.edu <dunn1pt@cmich.edu>
2017-03-13 12:01:57 -0400

    added new form

--- bis425_group9/Form1.Designer.vb
+++ bis425_group9/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(115, 70)
+        Me.Button1.Location = New System.Drawing.Point(52, 42)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
